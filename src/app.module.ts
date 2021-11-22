9c8cd8baa3914cf83f6a250e2218812d4225bf1d
Banktts <bankthamasaroch@gmail.com>
2021-11-22 20:54:34 +0700

    refactor to nest

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule} from '@nestjs/config';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ElectionModule} from "./Election/election.module";
+import {CreateElectionModule} from "./CreateElection/createElection.module";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: parseInt(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            entities: ['dist/**/*.entity{.ts,.js}'],
+            migrationsTableName: 'migration',
+            migrations: ["dist/migrations/*{.ts,.js}"],
+            cli: {
+                migrationsDir: 'src/migrations',
+            },
+            synchronize: true,
+            extra: {
+                ssl: {
+                    rejectUnauthorized: false,
+                },
+            },
+        }),
+        ElectionModule,
+        CreateElectionModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+      .setTitle("Elections service")
+      .setDescription('The Elections Service API Description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
